26d00cae5c0a1b8dd650b1563871ca61bfb9c997
albertxamlashe@gmail.com <albertxamlashe@gmail.com>
2023-01-17 07:25:00 +0000

    selecting first and last name from fullname

--- models/global_mart_raw/global_data_mart_product.sql
+++ models/global_mart_raw/global_data_mart_product.sql
@@ -1,2 +1,6 @@
-SELECT  
-*FROM `data-engineering-1-374714.dbt_xamlashe1425.product` 
+SELECT 
+        string_field_0 as Category,
+        string_field_1 as ProductID,
+        string_field_2 as ProductName,
+        string_field_3 as SubCategory
+FROM `data-engineering-1-374714.dbt_xamlashe1425.product` 

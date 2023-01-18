0314ba3c25672dd708f16d20a7c5c3259309279e
albertxamlashe@gmail.com <albertxamlashe@gmail.com>
2023-01-18 13:11:59 +0000

    created unique and not_null test

--- models/global_mart_raw/global_data_mart_customers.sql
+++ models/global_mart_raw/global_data_mart_customers.sql
@@ -9,7 +9,7 @@ WITH selecting_rows_after_second_row as
                 FROM {{ source('globalmart', 'customers') }}
                 WHERE string_field_0 !='CustomerID' )
 
-SELECT string_field_0 AS   CustomerID,
+SELECT string_field_0  AS    CustomerID,
         string_field_1 AS   CustomerName,
         string_field_2 AS   Segment,
         string_field_3 AS   Country,
